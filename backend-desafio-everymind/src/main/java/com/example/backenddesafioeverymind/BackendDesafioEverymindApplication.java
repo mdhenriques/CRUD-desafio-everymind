a5eb652c6d0f667ee20468a38d377e1165ac8212
Murilo Duarte <md.henriques@outlook.com>
2024-09-01 19:48:30 -0300

    adicionando backend

---
+++ backend-desafio-everymind/src/main/java/com/example/backenddesafioeverymind/BackendDesafioEverymindApplication.java
@@ -0,0 +1,13 @@
+package com.example.backenddesafioeverymind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDesafioEverymindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDesafioEverymindApplication.class, args);
+	}
+
+}
---
+++ backend-desafio-everymind/src/test/java/com/example/backenddesafioeverymind/BackendDesafioEverymindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backenddesafioeverymind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDesafioEverymindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
